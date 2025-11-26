4df99ccc470c64ba462ba025e4d388200109dbfc
axcellsz <122135545+axcellsz@users.noreply.github.com>
2025-11-26 20:32:57 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,11 @@
 
+export default {
+  async fetch(request, env, ctx) {
+    return new Response("hallo babi", {
+      status: 200,
+      headers: {
+        "content-type": "text/plain; charset=utf-8",
+      },
+    });
+  },
+};
