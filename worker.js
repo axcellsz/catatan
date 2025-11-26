f27e3762451165b87a6400dce001558a3f3b10e8
axcellsz <122135545+axcellsz@users.noreply.github.com>
2025-11-26 21:15:18 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,22 @@
-
 export default {
   async fetch(request, env, ctx) {
-    return new Response("hallo babi", {
+    // Ambil file index.html dari KV binding "PAGES"
+    const html = await env.PAGES.get("index.html");
+
+    if (!html) {
+      // Kalau key-nya belum ada di KV
+      return new Response("index.html tidak ditemukan di KV", {
+        status: 404,
+        headers: {
+          "content-type": "text/plain; charset=utf-8",
+        },
+      });
+    }
+
+    return new Response(html, {
       status: 200,
       headers: {
-        "content-type": "text/plain; charset=utf-8",
+        "content-type": "text/html; charset=utf-8",
       },
     });
   },
